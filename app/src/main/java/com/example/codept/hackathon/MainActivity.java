7548691030bacf1cfa2c4e6ba3c7155e71b26a3d
codept <lazybillla@gmail.com>
2018-03-16 14:46:19 +0530

    ssd

--- app/src/main/java/com/example/codept/hackathon/MainActivity.java
+++ app/src/main/java/com/example/codept/hackathon/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.codept.hackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static com.example.codept.hackathon.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    public void Register(android.view.View view){
+
+        Intent i=new Intent(MainActivity.this,Register.class);
+
+        startActivity(i);
+
+
     }
 }
